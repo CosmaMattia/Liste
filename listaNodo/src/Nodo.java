cefd6686a1912268cc905d922a6cdd2fd9fd8bb2
Cosma Mattia <antonio.cosma08@gmail.com>
2026-01-05 12:19:15 +0100

    Refactor Nodo class to include prev property

--- listaNodo/src/Nodo.java
+++ listaNodo/src/Nodo.java
@@ -1,23 +1,35 @@
 public class Nodo {
-    
+
     private String valore;
     private Nodo next;
-    
+    private Nodo prev;
+
     public Nodo(String valore) {
         this.valore = valore;
         this.next = null;
+        this.prev = null;
     }
-    
+
+    public Nodo getNext() {
+        return next;
+    }
+
     public void setNext(Nodo next) {
         this.next = next;
     }
-    public Nodo getNext() {
-        return this.next;
+
+    public Nodo getPrev() {
+        return prev;
     }
-    
+
+    public void setPrev(Nodo prev) {
+        this.prev = prev;
+    }
+
     public String getValore() {
-        return this.valore;
+        return valore;
     }
+
     public void setValore(String valore) {
         this.valore = valore;
     }
